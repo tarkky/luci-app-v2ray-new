7044a932cfa948e6478dca3800e801c9c4c2ff8d
BI7PRK <im@bi7prk.com>
2021-12-22 13:29:18 +0800

    add observatory settings

--- src/view/v2ray/observatory.ts
+++ src/view/v2ray/observatory.ts
@@ -14,32 +14,28 @@
 
 // @ts-ignore
 return L.view.extend<SectionItem[][]>({
-    render: function () {
-        const m = new form.Map(
-            "v2ray",
-            "%s - %s".format(_("V2Ray"), _("Observatory")),
-            _("Details: %s").format(
-                '<a href="https://www.v2fly.org/config/observatory.html#observatoryobject" target="_blank">ObservatoryObject</a>'
-            )
-        );
+  render: function () {
+    const m = new form.Map(
+      "v2ray",
+      "%s - %s".format(_("V2Ray"), _("Observatory")),
+      _("Details: %s").format(
+        '<a href="https://www.v2fly.org/config/observatory.html#observatoryobject" target="_blank">ObservatoryObject</a>'
+      )
+    );
 
-        const s1 = m.section(form.NamedSection, "main_observatory", "observatory");
-        s1.addremove = false;
+    const s1 = m.section(form.NamedSection, "main_observatory", "observatory");
+    s1.addremove = false;
 
-        let o;
-        o = s1.option(form.Flag, "enabled", _("Enabled"));
-        o.rmempty = false;
+    let o;
+    o = s1.option(form.Flag, "enabled", _("Enabled"));
+    o.rmempty = false;
 
-        o = s1.option(form.Value, "probeURL", _("ProbeURL"));
+    o = s1.option(form.Value, "probeURL", _("ProbeURL"));
 
-        o = s1.option(form.Value, "probeInterval", _("ProbeInterval"));
+    o = s1.option(form.Value, "probeInterval", _("ProbeInterval"));
 
-        o = s1.option(
-            form.DynamicList,
-            "subjectSelector",
-            _("SubjectSelector")
-        );
+    o = s1.option(form.DynamicList, "subjectSelector", _("SubjectSelector"));
 
-        return m.render();
-    },
+    return m.render();
+  },
 });
